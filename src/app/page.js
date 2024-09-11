ed526c02bd1e3d0274f15877c3f91688b709a9fb
Tatiana Vasylieva <tatiana@Tatianas-Laptop.local>
2024-09-11 02:18:20 +0200

    Add margin

--- src/app/page.js
+++ src/app/page.js
@@ -71,106 +71,108 @@ export default function Home() {
           /link shortener
         </p>
       </div>
+      <div className="mb-8">
+        <div className="middle-div mb-10 flex-grow flex flex-col items-center">
+          <div className="flex justify-start w-full lg:w-3/4 xl:w-2/3 2xl:w-1/2 mb-4">
+            <button
+              onClick={() =>
+                setOriginalUrl(
+                  "https://en.wikipedia.org/wiki/List_of_tallest_buildings_and_structures_in_the_Paris_region"
+                )
+              }
+              className="text-xs md:text-sm lg:text-sm xl:text-sm 2xl:text-sm bg-transparent px-4 py-2 text-gray-700 rounded-full border border-gray-400"
+            >
+              Add example link
+            </button>
+          </div>
+          <div className="flex flex-col lg:flex-row items-center space-y-4 lg:space-y-0 lg:space-x-4 mb-4 w-full lg:w-3/4 xl:w-2/3 2xl:w-1/2">
+            <input
+              type="text"
+              value={originalUrl}
+              onChange={(e) => setOriginalUrl(e.target.value)}
+              placeholder="Enter the link"
+              required
+              className={
+                "placeholder-gray-500 text-sm md:text-base lg:text-base xl:text-base 2xl:text-base border px-6 md:px-8 lg:px-8 xl:px-8 2xl:px-8 h-14 shadow-lg rounded-full w-full lg:flex-grow focus:outline-none"
+              }
+            />
+            <motion.button
+              whileHover={validateUrl(originalUrl) ? { scale: 1.05 } : {}}
+              whileTap={{ scale: 0.95 }}
+              onClick={handleSubmit}
+              type="button"
+              className={`text-sm md:text-base lg:text-base xl:text-base 2xl:text-base w-full lg:w-auto px-8 py-4 bg-customBlack shadow-lg rounded-full text-white text-medium transition-transform duration-200 ${
+                !validateUrl(originalUrl) ? "cursor-not-allowed" : ""
+              }`}
+              disabled={!validateUrl(originalUrl)}
+            >
+              Shorten
+            </motion.button>
+          </div>
 
-      <div className="middle-div mb-10 flex-grow flex flex-col items-center justify-center">
-        <div className="flex justify-start w-full lg:w-3/4 xl:w-2/3 2xl:w-1/2 mb-4">
-          <button
-            onClick={() =>
-              setOriginalUrl(
-                "https://en.wikipedia.org/wiki/List_of_tallest_buildings_and_structures_in_the_Paris_region"
-              )
-            }
-            className="text-xs md:text-sm lg:text-sm xl:text-sm 2xl:text-sm bg-transparent px-4 py-2 text-gray-700 rounded-full border border-gray-400"
-          >
-            <FontAwesomeIcon icon={faHashtag} className="mr-1" />
-            Example link
-          </button>
-        </div>
-        <div className="flex flex-col lg:flex-row items-center space-y-4 lg:space-y-0 lg:space-x-4 mb-4 w-full lg:w-3/4 xl:w-2/3 2xl:w-1/2">
-          <input
-            type="text"
-            value={originalUrl}
-            onChange={(e) => setOriginalUrl(e.target.value)}
-            placeholder="Enter the link"
-            required
-            className={
-              "placeholder-gray-500 text-sm md:text-base lg:text-base xl:text-base 2xl:text-base border px-6 md:px-8 lg:px-8 xl:px-8 2xl:px-8 h-14 shadow-lg rounded-full w-full lg:flex-grow focus:outline-none"
-            }
-          />
-          <motion.button
-            whileHover={validateUrl(originalUrl) ? { scale: 1.05 } : {}}
-            whileTap={{ scale: 0.95 }}
-            onClick={handleSubmit}
-            type="button"
-            className={`text-sm md:text-base lg:text-base xl:text-base 2xl:text-base w-full lg:w-auto px-8 py-4 bg-customBlack shadow-lg rounded-full text-white text-medium transition-transform duration-200 ${
-              !validateUrl(originalUrl) ? "cursor-not-allowed" : ""
-            }`}
-            disabled={!validateUrl(originalUrl)}
-          >
-            Shorten
-          </motion.button>
-        </div>
-
-        {!isValidUrl && (
-          <p className="text-red-500 text-sm mt-2">Please enter a valid URL.</p>
-        )}
-
-        <div className="relative mt-1 w-full lg:w-3/4 xl:w-2/3 2xl:w-1/2">
-          {loading ? (
-            <div className="flex justify-center space-x-1">
-              <div className="w-2 h-2 lg:w-3 lg:h-3 xl:w-3 xl:h-3 2xl:w-3 2xl:h-3 bg-gray-500 rounded-full dot"></div>
-              <div className="w-2 h-2 lg:w-3 lg:h-3 xl:w-3 xl:h-3 2xl:w-3 2xl:h-3 bg-gray-500 rounded-full dot"></div>
-              <div className="w-2 h-2 lg:w-3 lg:h-3 xl:w-3 xl:h-3 2xl:w-3 2xl:h-3 bg-gray-500 rounded-full dot"></div>
-            </div>
-          ) : (
-            shortUrl && (
-              <motion.div
-                initial={{ opacity: 0, y: 10 }}
-                animate={{ opacity: 1, y: 0 }}
-                exit={{ opacity: 0, y: 10 }}
-                transition={{ duration: 0.3 }}
-                className="absolute inset-x-0 border border-black flex items-center bg-transparent rounded-full h-14 pl-6 pr-0 md:pl-8 md:pr-1 lg:pl-8 lg:pr-1 xl:pl-8 xl:pr-1 2xl:pl-8 2xl:pr-1 w-full"
-              >
-                <div className="text-sm md:text-base lg:text-base xl:text-base 2xl:text-base flex-1 flex items-center space-x-2 overflow-hidden">
-                  <FontAwesomeIcon
-                    icon={faLink}
-                    className="text-customOrange mr-1"
-                  />
-                  <a
-                    href={shortUrl}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="truncate whitespace-nowrap overflow-hidden"
-                  >
-                    {shortUrl}
-                  </a>
-                </div>
-
-                <button
-                  onClick={handleCopy}
-                  className="text-sm md:text-base lg:text-base xl:text-base 2xl:text-base px-5 py-3 md:px-6 lg:px-6 xl:px-6 2xl:px-6 border-l border-black flex items-center space-x-2 h-full"
-                >
-                  {isCopied ? (
-                    <>
-                      <FontAwesomeIcon
-                        icon={faCheck}
-                        className="text-customOrange"
-                      />
-                      <span className="hidden lg:inline-block">Copied!</span>
-                    </>
-                  ) : (
-                    <>
-                      <FontAwesomeIcon
-                        icon={faCopy}
-                        className="text-customOrange"
-                      />
-                      <span className="hidden lg:inline-block">Copy</span>
-                    </>
-                  )}
-                </button>
-              </motion.div>
-            )
+          {!isValidUrl && (
+            <p className="text-red-500 text-sm mt-2">
+              Please enter a valid URL.
+            </p>
           )}
+
+          <div className="relative mt-1 w-full lg:w-3/4 xl:w-2/3 2xl:w-1/2">
+            {loading ? (
+              <div className="flex justify-center space-x-1">
+                <div className="w-2 h-2 lg:w-3 lg:h-3 xl:w-3 xl:h-3 2xl:w-3 2xl:h-3 bg-gray-500 rounded-full dot"></div>
+                <div className="w-2 h-2 lg:w-3 lg:h-3 xl:w-3 xl:h-3 2xl:w-3 2xl:h-3 bg-gray-500 rounded-full dot"></div>
+                <div className="w-2 h-2 lg:w-3 lg:h-3 xl:w-3 xl:h-3 2xl:w-3 2xl:h-3 bg-gray-500 rounded-full dot"></div>
+              </div>
+            ) : (
+              shortUrl && (
+                <motion.div
+                  initial={{ opacity: 0, y: 10 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: 10 }}
+                  transition={{ duration: 0.3 }}
+                  className="absolute inset-x-0 border border-black flex items-center bg-transparent rounded-full h-14 pl-6 pr-0 md:pl-8 md:pr-1 lg:pl-8 lg:pr-1 xl:pl-8 xl:pr-1 2xl:pl-8 2xl:pr-1 w-full"
+                >
+                  <div className="text-sm md:text-base lg:text-base xl:text-base 2xl:text-base flex-1 flex items-center space-x-2 overflow-hidden">
+                    <FontAwesomeIcon
+                      icon={faLink}
+                      className="text-customOrange mr-1"
+                    />
+                    <a
+                      href={shortUrl}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="truncate whitespace-nowrap overflow-hidden"
+                    >
+                      {shortUrl}
+                    </a>
+                  </div>
+
+                  <button
+                    onClick={handleCopy}
+                    className="text-sm md:text-base lg:text-base xl:text-base 2xl:text-base px-5 py-3 md:px-6 lg:px-6 xl:px-6 2xl:px-6 border-l border-black flex items-center space-x-2 h-full"
+                  >
+                    {isCopied ? (
+                      <>
+                        <FontAwesomeIcon
+                          icon={faCheck}
+                          className="text-customOrange"
+                        />
+                        <span className="hidden lg:inline-block">Copied!</span>
+                      </>
+                    ) : (
+                      <>
+                        <FontAwesomeIcon
+                          icon={faCopy}
+                          className="text-customOrange"
+                        />
+                        <span className="hidden lg:inline-block">Copy</span>
+                      </>
+                    )}
+                  </button>
+                </motion.div>
+              )
+            )}
+          </div>
         </div>
       </div>
 
